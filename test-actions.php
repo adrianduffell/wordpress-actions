ab1f78718360fe53b03a2e1bd95592efe05e0f5c
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2020-04-30 12:25:10 +0800

    Add plugin scaffold

---
+++ test-actions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Test Actions
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     test-actions
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Test_Actions
+ */
+
+// Your code starts here.
